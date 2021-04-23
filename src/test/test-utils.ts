00528714a40da1685a12f43376e7bf6501d4076f
Brandon Scott <bscott@andculture.com>
2021-04-23 13:51:39 -0400

    Scaffold out set, reset, and merge config functions for testing

--- src/test/test-utils.ts
+++ src/test/test-utils.ts
@@ -1,4 +1,39 @@
 import { ExtensionConfiguration } from "../interfaces/extension-configuration";
+import * as vscode from "vscode";
+import { ConfigUtils } from "../utilities/config-utils";
 
-const setConfiguration = async (config: ExtensionConfiguration) => {};
-const resetConfiguration = async () => {};
+// -----------------------------------------------------------------------------------------
+// #region Constants
+// -----------------------------------------------------------------------------------------
+
+const { key, defaultConfig } = ConfigUtils;
+
+// #endregion Constants
+
+// -----------------------------------------------------------------------------------------
+// #region Public Functions
+// -----------------------------------------------------------------------------------------
+
+const TestUtils = {
+    async mergeConfig(updated: Partial<ExtensionConfiguration>) {
+        const existing = await ConfigUtils.get();
+        return this.setConfig({ ...existing, ...updated });
+    },
+    async setConfig(config: ExtensionConfiguration) {
+        return vscode.workspace
+            .getConfiguration()
+            .update(key, config, vscode.ConfigurationTarget.Global);
+    },
+    async resetConfig() {
+        return this.setConfig(defaultConfig);
+    },
+};
+// #endregion Public Functions
+
+// -----------------------------------------------------------------------------------------
+// #region Exports
+// -----------------------------------------------------------------------------------------
+
+export { TestUtils };
+
+// #endregion Exports
